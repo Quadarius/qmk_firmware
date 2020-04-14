3e5a97dad97079cff0d9d61d244c5013cb3f3572
Quadarius Moore <qkmoore@crimson.ua.edu>
2020-04-14 13:47:19 -0500

    Create qmoore/keymap.c

---
+++ keyboards/crkbd/keymaps/qmoore/keymap.c
@@ -0,0 +1,224 @@
+#include QMK_KEYBOARD_H
+#include "bootloader.h"
+#ifdef PROTOCOL_LUFA
+  #include "lufa.h"
+  #include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "ssd1306.h"
+#endif
+
+extern keymap_config_t keymap_config;
+
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+
+extern uint8_t is_master;
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  CODE,
+  LOWER,
+  RAISE,
+  ADJUST
+};
+
+#define KC______ KC_TRNS
+#define KC_XXXXX KC_NO
+#define KC_RESET RESET
+#define KC_LOWER LOWER
+#define KC_RAISE RAISE
+
+#define KC_CTLTB CTL_T(KC_TAB)
+#define KC_CTLEN CTL_T(KC_ENT)
+#define KC_CQUO  LT(_CODE, KC_QUOT)
+#define KC_ESATL ALT_T(KC_ESC)
+
+#define CTLENT CTL_T(KC_ENT)
+#define ALTSPC ALT_T(KC_SPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        TAB,     Q,     W,     E,     R,     T,                      Y,     U,     I,     O,     P,  BSPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      ESCTL,     A,     S,     D,     F,     G,                      H,     J,     K,     L,  SCLN,  CQUO,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       LSFT,     Z,     X,     C,     V,     B,                      N,     M,  COMM,   DOT,  SLSH,  RSFT,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  LGUI, LOWER,   SPC,    CLTEN, RAISE, RALT \
+                              //`--------------------'  `--------------------'
+  ),
+
+    [_CODE] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+      _____, TILDE, _____, _____, _____, _____,                   PIPE,  LBRC,  RBRC, _____, _____,   DEL,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      _____, TILDE,   GRV,  PLUS,  MINS,   EQL,                   BSLS,  LPRN,  RPRN, _____, _____, _____,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      _____, _____, _____, _____,  UNDS, _____,                  _____,  LCBR,  RCBR, _____, _____, _____,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  _____, _____, _____,    _____, _____, _____\
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_LOWER] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+      _____,     1,     2,     3,     4,     5,                   CIRC,  AMPR,  ASTR,  LPRN,  RPRN,  PIPE,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      _____,     6,     7,     8,     9,     0,                   EXLM,    AT,  HASH,   DLR,  PERC,  BSLS,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      _____, _____, _____, _____,  UNDS, _____,                  _____, _____, _____, _____, _____, _____,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  _____, _____, _____,    _____, _____, _____\
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_RAISE] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+      _____,     1,     2,     3,     4,     5,                      6,     7,     8,     9,     0,   DEL, \
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      _____,  TILD,  UNDS,  PLUS,  MINS,   EQL,                   LEFT,  DOWN,    UP,  RGHT, COLON,  DQUO,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      _____, _____, _____, _____,  UNDS, _____,                   HOME,   END,    LT,    GT,  QUES, _____,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  _____, _____, _____,    _____, _____, _____\
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_ADJUST] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+      RESET,    F1,    F2,    F3,    F4,    F5,                   PSCR,  SLCK, PAUSE,   INS,   ESC,   DEL,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      CTLTB,    F6,    F7,    F8,    F9,   F10,                   VOLU,  MPRV,  MPLY,  MNXT, XXXXX, _____,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      LSFT,    F11,   F12, XXXXX, XXXXX, XXXXX,                   VOLD,  RCTL,  RALT,  RGUI, XXXXX, _____,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  _____, _____, _____,    _____, _____, _____\
+                              //`--------------------'  `--------------------'
+  )
+};
+
+int RGB_current_mode;
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+// Setting ADJUST layer RGB back to default
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+    layer_on(layer3);
+  } else {
+    layer_off(layer3);
+  }
+}
+
+void matrix_init_user(void) {
+    #ifdef RGBLIGHT_ENABLE
+      RGB_current_mode = rgblight_config.mode;
+    #endif
+    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+    #ifdef SSD1306OLED
+        iota_gfx_init(!has_usb());   // turns on the display
+    #endif
+}
+
+//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
+#ifdef SSD1306OLED
+
+// When add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+void set_timelog(void);
+const char *read_timelog(void);
+
+void matrix_scan_user(void) {
+   iota_gfx_task();
+}
+
+void matrix_render_user(struct CharacterMatrix *matrix) {
+  if (is_master) {
+    // If you want to change the display of OLED, you need to change here
+    matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, read_keylog());
+    //matrix_write_ln(matrix, read_keylogs());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_RALT_lgui));
+    //matrix_write_ln(matrix, read_host_led_state());
+    matrix_write_ln(matrix, read_timelog());
+  } else {
+    matrix_write(matrix, read_logo());
+  }
+}
+
+void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+    memcpy(dest->display, source->display, sizeof(dest->display));
+    dest->dirty = true;
+  }
+}
+
+void iota_gfx_task_user(void) {
+  struct CharacterMatrix matrix;
+  matrix_clear(&matrix);
+  matrix_render_user(&matrix);
+  matrix_update(&display, &matrix);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef SSD1306OLED
+    set_keylog(keycode, record);
+#endif	
+    set_timelog();
+  }
+
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+        if (record->event.pressed) {
+          layer_on(_ADJUST);
+        } else {
+          layer_off(_ADJUST);
+        }
+        return false;
+        break;
+  }
+  return true;
+}
+
+#endif
